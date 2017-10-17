3c0de79b2dd92c51b308b4f176ee41d3c5b56035
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-17 08:56:53 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,10 +1,11 @@
 class Patient
-  attr_accessor :name, :appointments
+  attr_accessor :name, :appointments, :doctors
 
 
   def initialize(name)
     self.name = name
     self.appointments = []
+    self.doctors = appointment.doc
   end
 
   def add_appointment(appointment)
