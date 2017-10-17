861d0b7d99bdee95ce01fb4bd00fb454b61ff794
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-17 09:02:58 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -5,12 +5,13 @@ class Patient
   def initialize(name)
     self.name = name
     self.appointments = []
+    self.doctors = []
   end
 
   def add_appointment(appointment)
     appointments << appointment
     appointment.patient = self
-    self.appointments << appointment.doctor
+    
   end
 
 end
